9355181a8beff7930a29ae7be070e2b674bb2aae
Fabrinio <fabriniolucas1412@gmail.com>
2022-09-21 21:56:36 -0300

    Aula3

---
+++ Aula3/js/aula1.js
@@ -0,0 +1,21 @@
+//linguagem não tipada
+10;
+10.05;
+//boolean
+true
+false
+
+//aray
+[10,10.19, "oi", true]
+
+//objeto
+var aluno={nome:'Lucas',
+sobrenome:'Fabrinio'};
+
+//tipagem dinamica
+var y=15
+    y="oi"
+
+var y=undefined
+
+var aluno = null
